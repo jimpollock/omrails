130a544c9097c9b247ab8bc0a8604fd54517d453
Jim Pollock <jp@pollockprints.com>
2013-09-11 21:29:06 -0500

    Add Paperclip Gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,12 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
-  
-  validates :description, presence: true
+  attr_accessible :description, :image # This line won't work in Rails 4, instead, you'll have to do what I show below
+
+    validates :description, presence: true
+    validates :user_id, presence: true
+    has_attached_file :image, styles: { medium: "320x240>", :thumb => "100x100>"}
+    validates_attachment :image, presence: true,
+                              content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                              size: { less_than: 5.megabytes }
+    belongs_to :user
+
 end
